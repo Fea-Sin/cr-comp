c3d91e66e0c4fc056ff63294e286c0e9ae020bc5
Fea-Sin <1210992614@qq.com>
2019-11-29 11:54:31 +0800

    优化项目

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import { mount, render } from 'enzyme'
-import { renderToJson } from 'enzyme-to-json'
-import Comp from '../src'
+import React from 'react';
+import { mount, render } from 'enzyme';
+import { renderToJson } from 'enzyme-to-json';
+import Comp from '../src';
 
 
 describe('cr-comp', () => {
